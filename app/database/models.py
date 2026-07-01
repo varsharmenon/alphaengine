14c077a463bebf9bd84cdfa050471bd7f9ab920c
varsharmenon <raingirlhere@gmail.com>
2026-07-01 23:07:01 +0530

    Add Price model and stock-price relationships

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Date, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
 from .database import Base
 
 class Stock(Base):
@@ -15,3 +17,24 @@ class Stock(Base):
     series = Column(String)
 
     isin_code = Column(String, unique=True)
+
+    prices = relationship("Price", back_populates="stock")
+
+
+class Price(Base):
+    __tablename__ = "prices"
+
+    id = Column(Integer, primary_key=True)
+
+    stock_id = Column(Integer, ForeignKey("stocks.id"), nullable=False, index=True)
+
+    date = Column(Date)
+
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+
+    volume = Column(Integer)
+
+    stock = relationship("Stock", back_populates="prices")
